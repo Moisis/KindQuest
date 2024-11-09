ec450ffaa5c0fea14e7a62cbcb0db3a37e10f297
DevKiroBerty <kirollos.rezk@gmail.com>
2024-11-10 00:46:53 +0200

    Added user functionality

--- app/models/Auth/IndividualAuth.php
+++ app/models/Auth/IndividualAuth.php
@@ -1,6 +1,10 @@
 <?php
 
 
+require_once "AuthStrategy.php";
+require_once __DIR__."/../../../../core/Database.php";
+
+
 class IndividualAuth implements AuthStrategy
 {
 
@@ -11,7 +15,15 @@ class IndividualAuth implements AuthStrategy
 
     public function login(array $credentials): bool
     {
-        // TODO: Implement login() method.
+        $db = Database::getConnection(); // Access database connection directly
+
+        $query = "SELECT * FROM Account WHERE username = :username AND password = :password AND account_type = 'user'";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':username', $credentials['username']);
+        $stmt->bindParam(':password', $credentials['password']);
+        $stmt->execute();
+
+        return $stmt->rowCount() > 0;
     }
 
     public function logout(): void
--- app/models/Users/Admin.php
+++ app/models/Users/Admin.php
@@ -2,15 +2,34 @@
 
 declare(strict_types=1);
 
-class Admin{
+class Admin extends BaseAccount{
 
-    public function login(string $userName, string $password){
-        //authenticate with db
-        //returns boolean
+
+    public function __construct(string $userName, string $password){
+
+        $this->userID = rand(1,999);
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->auth = new AdminAuth();
+
     }
 
-    
 
+    public function setAuthStrategy(AuthStrategy $auth): void
+    {
+        $this->auth = $auth;
+    }
+
+    public function login(array $credentials): bool {
+
+        return $this->auth->login($credentials);
+
+    }
+
+    public function register(array $data): bool
+    {
+        return $this->auth->register($data);
+    } 
 
 }
 
