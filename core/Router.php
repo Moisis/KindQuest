0d44d9ff0e6d62f6068d6e89216b66f20e9199d3
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-11-12 15:28:25 +0200

    update required file

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,7 @@
 
 require_once __DIR__ . '/../app/controllers/AboutController.php';
 require_once __DIR__ . '/../app/controllers/HomeController.php';
-require_once __DIR__ . '/../app/controllers/EventController.php';
+require_once __DIR__ . '/../app/controllers/Event/EventController.php';
 
 //Auth
 require_once __DIR__ . '/../app/controllers/Auth/RegisterController.php';
