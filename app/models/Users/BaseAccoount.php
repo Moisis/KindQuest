336cba893a1a7d023d3b3ebe494ab0a9230bf689
DevKiroBerty <kirollos.rezk@gmail.com>
2024-11-16 23:11:56 +0200

    Added organization Authentication

--- app/models/Users/BaseAccoount.php
+++ app/models/Users/BaseAccoount.php
@@ -2,9 +2,42 @@
 
 declare(strict_types=1);
 abstract class BaseAccount{
+
+    protected AuthStrategy $auth;
     protected int $userID;
     protected string $userName;
-    protected string $password;    
+    protected string $password;
+    protected string $accountType;  
+    
+    public abstract function setAuthStrategy (AuthStrategy $auth) : void;
+
+    public abstract function login(array $credentials): bool;
+
+    public abstract function register(array $data): bool;
+
+
+    public static function getUserById(int $accountId): ?array {
+        $query = "SELECT account_id, username, account_type FROM Account WHERE account_id = ?";
+        $result = run_select_query($query, [$accountId]);
+
+        if ($result !== null && !empty($result)) {
+            return $result[0];
+
+        } else {
+            
+            return null;
+        }
+    }
+
+
+    public function getUserId(): int {
+        return $this->userID;
+    }
+
+
+    
+
+    
 
 }
 
